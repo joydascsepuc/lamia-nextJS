525f80a2dc9373b33ff509c619288f2d6d6e4f97
Joy Das <joydascsepuc@gmail.com>
2023-08-04 22:01:17 +0600

    Added all required pages and header and footer components

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div>
+            <h5>&copy;Lamia, All rights reserved.</h5>
+            <div>
+                {/* <Image > */}
+            </div>
+        </div>
+    )
+};
+
+export default Footer;
