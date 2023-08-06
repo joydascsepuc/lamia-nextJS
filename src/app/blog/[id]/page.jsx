9325cdfde8eae4e734134fd3d18801a86cff0f9f
Joy Das <joydascsepuc@gmail.com>
2023-08-06 22:59:50 +0600

    Updated blog details post with dynamic post id and userID

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,41 +1,93 @@
 import Image from "next/image";
 import React from "react";
 
-const BlogPost = () => {
+async function getData(id) {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts/' + id, {
+        next: {
+            revalidate: 10
+        }
+    });
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+
+    return res.json();
+}
+
+async function getUserData(id) {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id, {
+        next: {
+            revalidate: 10
+        }
+    });
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+
+    return res.json();
+}
+
+const images = [
+    {
+        imageURL: 'https://images.pexels.com/photos/478544/pexels-photo-478544.jpeg?auto=compress&cs=tinysrgb&w=900',
+    },
+    {
+        imageURL: 'https://images.pexels.com/photos/1006073/pexels-photo-1006073.jpeg?auto=compress&cs=tinysrgb&w=900',
+    },
+    {
+        imageURL: 'https://images.pexels.com/photos/371160/pexels-photo-371160.jpeg?auto=compress&cs=tinysrgb&w=900',
+    },
+    {
+        imageURL: 'https://images.pexels.com/photos/1130624/pexels-photo-1130624.jpeg?auto=compress&cs=tinysrgb&w=900',
+    },
+    {
+        imageURL: 'https://images.pexels.com/photos/693429/pexels-photo-693429.jpeg?auto=compress&cs=tinysrgb&w=900',
+    },
+];
+
+const BlogPost = async ({ params }) => {
+
+    const data = await getData(params.id);
+    const userData = await getUserData(data.userId);
+    const randomImageIndex = Math.floor(Math.random() * images.length);
+    const randomImage = (randomImageIndex, images[randomImageIndex]);
+
     return (
         <div className="my-11">
             <div className="flex flex-row-reverse gap-5 justify-center">
                 <div className="flex-1">
                     <div className="h-[300px] w-full relative">
-                        <Image src={'https://images.pexels.com/photos/1130624/pexels-photo-1130624.jpeg?auto=compress&cs=tinysrgb&w=900'} fill={true} alt="blog-image" className="object-cover" />
+                        <Image src={randomImage.imageURL} fill={true} alt="blog-image" className="object-cover" />
                     </div>
                 </div>
                 <div className="flex-1 flex flex-col gap-5">
-                    <h1 className="text-3xl font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, maiores?</h1>
+                    <h1 className="text-3xl font-bold">{data.title}</h1>
                     <p className="text-md">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos temporibus possimus officiis accusantium impedit tempore tempora aperiam dolorum quaerat ad perferendis totam velit magnam quidem minima, eum esse adipisci blanditiis illo iure itaque. Nihil aliquam odio, animi ad a reiciendis, omnis dolore neque tempore quae eligendi, alias corrupti veniam suscipit
+                        {data.body}
                     </p>
                     <div className="flex mt-5 gap-2">
                         <div className="">
                             <div className="w-[30px] h-[30px] relative">
-                                <Image src={'https://images.pexels.com/photos/1130624/pexels-photo-1130624.jpeg?auto=compress&cs=tinysrgb&w=300'} className="rounded-full" fill={true} alt="user-icon" />
+                                <Image src={randomImage.imageURL} className="rounded-full" fill={true} alt="user-icon" />
                             </div>
                         </div>
                         <div className="">
-                            <h5 className="text-md font-bold">Tasnim Kafa</h5>
+                            <h5 className="text-md font-bold">{userData.name}</h5>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="mt-20">
                 <p className="text-md">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo temporibus numquam eum, officiis aliquam aperiam totam provident at soluta facilis asperiores labore nesciunt tempore est laboriosam? Minima dignissimos quisquam animi deserunt omnis repudiandae aperiam vel iste dolorem. Quia, suscipit ut.
+                    {data.body}
                     <br /><br />
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ipsum incidunt unde. Voluptatem optio eius mollitia reiciendis non voluptatibus deleniti magni rem, ratione iure, modi asperiores numquam hic ipsa? Et, magni repellat vitae reprehenderit saepe asperiores temporibus aliquid laudantium sequi, tenetur earum tempora facere quia dolor excepturi aperiam eos deserunt quam odit cum eaque optio quo! Ipsam sint perspiciatis eveniet.
+                    {data.body}
                     <br /><br /><br />
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur maxime autem vitae aliquid ut sed quo atque voluptatibus nisi veniam.
+                    {data.body}
                     <br /><br />
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sit, sint veritatis saepe blanditiis dolores non, repellat odit culpa qui eum, magni provident amet quo pariatur tempora aut! Omnis aliquid quisquam, dolor sed dignissimos quaerat. Veritatis eveniet cupiditate quibusdam porro odit nostrum, magnam facilis fuga quia. Vitae odio voluptatum temporibus, itaque debitis rerum repellendus placeat dolorem quis fugit ut possimus corporis corrupti mollitia ab reiciendis? Dolore reprehenderit voluptates excepturi. Nesciunt maxime ea hic nostrum explicabo harum unde. Molestias nam voluptas vero ex ea esse assumenda mollitia nisi, consequatur aspernatur perspiciatis minima reiciendis consectetur! Accusantium labore asperiores non, voluptate quo nesciunt?
+                    {data.body}
                 </p>
             </div>
         </div>
