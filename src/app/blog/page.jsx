8b2000c2982bb3c594556396cc64b3b01c42d951
Joy Das <joydascsepuc@gmail.com>
2023-08-06 21:01:59 +0600

    Updated blog page to get data from the json placeholder

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,53 +2,54 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const data = [
+async function getData() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        next: {
+            revalidate: 10
+        }
+    });
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+    return res.json();
+}
+
+const images = [
     {
-        id: 1,
         imageURL: 'https://images.pexels.com/photos/478544/pexels-photo-478544.jpeg?auto=compress&cs=tinysrgb&w=900',
-        title: 'Lorem ipsum dolor sit amet.',
-        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus eius inventore cum repellendus, soluta atque quis assumenda veritatis libero praesentium!'
     },
     {
-        id: 2,
         imageURL: 'https://images.pexels.com/photos/1006073/pexels-photo-1006073.jpeg?auto=compress&cs=tinysrgb&w=900',
-        title: 'Lorem ipsum dolor sit amet.',
-        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus eius inventore cum repellendus, soluta atque quis assumenda veritatis libero praesentium!'
     },
     {
-        id: 3,
         imageURL: 'https://images.pexels.com/photos/371160/pexels-photo-371160.jpeg?auto=compress&cs=tinysrgb&w=900',
-        title: 'Lorem ipsum dolor sit amet.',
-        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus eius inventore cum repellendus, soluta atque quis assumenda veritatis libero praesentium!'
     },
     {
-        id: 4,
         imageURL: 'https://images.pexels.com/photos/1130624/pexels-photo-1130624.jpeg?auto=compress&cs=tinysrgb&w=900',
-        title: 'Lorem ipsum dolor sit amet.',
-        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus eius inventore cum repellendus, soluta atque quis assumenda veritatis libero praesentium!'
     },
     {
-        id: 5,
         imageURL: 'https://images.pexels.com/photos/693429/pexels-photo-693429.jpeg?auto=compress&cs=tinysrgb&w=900',
-        title: 'Lorem ipsum dolor sit amet.',
-        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus eius inventore cum repellendus, soluta atque quis assumenda veritatis libero praesentium!'
     },
 ];
 
-const Blog = () => {
+const Blog = async () => {
+    const data = await getData();
     return (
         <div>
             {
                 data.map((item) => {
+                    const randomImageIndex = Math.floor(Math.random() * images.length);
+                    const randomImage = (randomImageIndex, images[randomImageIndex]);
                     return (
                         <Link href={'/blog/' + item.id} key={item.id}>
                             <div className="flex my-10" >
                                 <div className="flex-1 h-[200px] w-[200px] relative">
-                                    <Image src={item.imageURL} alt="post-images" fill={true} className="object-cover" />
+                                    <Image src={randomImage.imageURL} alt="post-images" fill={true} className="object-cover" />
                                 </div>
                                 <div className="flex-1 px-12 flex flex-col gap-5 justify-center">
                                     <h2 className="text-2xl font-bold">{item.title}</h2>
-                                    <p className="text-md">{item.desc}</p>
+                                    <p className="text-md">{item.body}</p>
                                 </div>
                             </div>
                         </Link>
